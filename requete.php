fe6b914b8128971a4f8eaf7d4008b94f81c45d2f
Alex <alexben@live.fr>
2021-11-24 16:29:58 +0100

    page php

---
+++ requete.php
@@ -0,0 +1,68 @@
+<?php
+require_once("connexion.php");
+if(isset($_POST["todoSubmit"])){
+    $sql = "INSERT INTO todoapp(todo) VALUES (:todo)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        "todo" => $_POST["todo"]
+    ]);
+    header("location: index.php");
+    return;
+}
+
+if(isset($_POST["checkSubmit"])) {
+    $sql = "UPDATE todoapp SET is_check = :is_check WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        "id" => $_POST["todo_id"],
+        "is_check" => 1
+    ]);
+    header("location: index.php");
+    return;
+}
+
+if (isset($_POST["uncheckSubmit"])) {
+    $sql = "UPDATE todoapp SET is_check = :is_check WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        "id" => $_POST["todo_id"],
+        "is_check" => 0
+    ]);
+    header("location: index.php");
+    return;
+}
+
+if(isset($_POST["modify"]) && isset($_POST["todo_name"])) {
+    $sql = "UPDATE todoapp SET todo = :todo WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        "id" => $_POST["todo_id"],
+        "todo" => $_POST["todo_name"]
+    ]);
+    $_SESSION["edit"] = false;
+    header("location: index.php");
+    return;
+}
+
+if(isset($_POST["deleteSubmit"]) && isset($_POST["todo_id"])) {
+    $sql = "DELETE FROM todoapp WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        "id" => $_POST["todo_id"]
+    ]);
+    header("location: index.php");
+    return;
+}
+
+if(isset($_POST["deleteAllSubmit"])) {
+$sql = "TRUNCATE TABLE todoapp ";
+    $stmt = $pdo->query($sql);
+    header("location: index.php");
+    return;
+}
+
+if(isset($_POST["editSubmit"])) {
+    $_SESSION["edit"] = true;
+    $_SESSION["id"] = $_POST["todo_id"];
+    header("location:index.php");
+}
